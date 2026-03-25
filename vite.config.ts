88598e18c5e994990fe0bc40d17372fa8e114d46
Tobias Tornqvist <tobias@snoweb.info>
2026-03-24 18:56:50 -0700

    adding fallback route

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/snoweb/",
   build: {
     outDir: "docs",
     emptyOutDir: true,
